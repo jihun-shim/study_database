d4d691c13407df7882c51032ae48e3693c3766fd
jihun-shim <heartrains1@gmail.com>
2024-10-14 08:57:38 +0000

    실습

--- sqls/finance papers/finance_papers.py
+++ sqls/finance papers/finance_papers.py
@@ -16,11 +16,30 @@ try:
         # Read
         sql = "SELECT * FROM `question , answer`"
         cursor.execute(sql)
-        data = cursor.fetchall()
-        for row in data:
-            print(row)  # 각 행 출력
-            print(f"PK_A: {row['PK_A']}, column1: {row['question']}, column2: {row['answer']}")
-
-
+        data_q = cursor.fetchall()
+        sql = "SELECT * FROM `options`"
+        cursor.execute(sql)
+        data_o = cursor.fetchall()
+        
+        sum=0
+        result=[]
+        for quest in data_q:
+            print('**',quest['question'],'**')  # 각 행 출력
+            for opt in data_o: # 각 행 출력
+                if quest['PK_A'] == opt['FK_A']:
+                    print(opt['options'])
+            user=input('당신의 답변 (A, B, C 또는 D) :').upper()        
+            if user == quest['answer']:
+                print('정답입니다.',quest['answer'])
+                result.append('정답')
+                sum += 1         
+            else :
+                print('틀렸습니다.')
+                result.append('오답')
+        print('**퀴즈 완료! 당신의 총 점수는:' , sum ,'/5' , '**')        
 finally:
-    conn.close()
+    conn.close()                    
+            # print(f"PK_A: {row['PK_A']}, column1: {row['question']}, column2: {row['answer']}")        
+            # print(f"PK_B: {row1['PK_B']}, column1: {row1['FK_A']}, column2: {row1['options']}")
+
+
